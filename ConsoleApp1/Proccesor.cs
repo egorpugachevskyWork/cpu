38f5e55dbc4182de96e9a07d1b8567b6586e870f
RainbowX <pro100.phizik@gmail.com>
2021-11-09 00:48:52 +0300

    For lesha

---
+++ ConsoleApp1/Proccesor.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class UserTimes
+    {
+        public List<int> requests { get; set; }
+        public int isIO { get; set; }
+        public UserTimes(int[] times)
+        {
+            this.isIO = 0;
+            requests = new List<int>();
+            for (int i = 0; i < times.Length; ++i)
+            {
+                requests.Add(times[i]);
+            }
+        }
+    }
+
+    public class Proccesor
+    {
+        public int Tact { get; set; }
+        public int Io { get; set; }
+       
+        private List<Queue<UserTimes>> _priorities;
+        public Queue<UserTimes> this[int index]
+        {
+            get
+            {
+                return _priorities[index];
+            }
+        }
+
+        public Proccesor(int level, int tact, int io)
+        {
+            if (tact < 1)
+            {
+                Console.WriteLine("Tact must be bigger than 1");
+                return;
+            }
+
+            if (io < 0)
+            {
+                Console.WriteLine("Input/output time must be bigger or equal than 0");
+                return;
+            }
+
+            _priorities = new List<Queue<UserTimes>>(level);
+            this.Io = io;
+            this.Tact = tact;
+            for (int i = 0; i < level; ++i)
+            {
+                _priorities.Add(new Queue<UserTimes>());
+            }
+        }
+        public void AddUser(int priority, params int[] times)
+        {
+            if (priority < 0 || priority > _priorities.Count)
+            {
+                //The better to pass the delegate here
+                Console.WriteLine($"The priority must be positive and less than {_priorities.Count}");
+                return;
+            }
+
+            _priorities[priority].Enqueue(new UserTimes(times));
+        }
+
+        private int decIo(int j)
+        {
+            int count = 0;
+
+            for (int i = 0; i < _priorities[j].Count; ++i)
+            {
+                UserTimes user = _priorities[j].Peek();
+                if (user.isIO > 0)
+                {
+                    user.isIO = user.isIO - this.Tact;
+                    ++count;
+                }
+                else if (user.isIO < 0)
+                {
+                    user.isIO = 0;
+                }
+                //for (int k  =0; k < user.requests.Count; ++k)
+                //{
+                //    Console.WriteLine($"{user.requests[k]}");
+
+                //}
+                //Console.WriteLine("- - - - - - - -- - - - -- - - -");
+                _priorities[j].Enqueue(user);
+                _priorities[j].Dequeue();
+            }
+
+            return count;
+
+        }
+        public (int, int) getConsumingTime()
+        {
+            int gaps = 0, ticks = 0, totalGaps = 0;
+            int i = 0;
+
+            while (_priorities.Count != 0)
+            {
+                while (_priorities[i].Count != 0)
+                {
+                    int count = this.decIo(i);//The problem is here. Cause I need to reduce Io in every user
+                    if (count == _priorities[i].Count && i != _priorities.Count - 1)
+                    {
+                        i = (i + 1) % _priorities.Count;
+                        break;
+                    }
+                    UserTimes user = _priorities[i].Peek();
+                    while (user.isIO != 0 && count != _priorities[i].Count)
+                    {
+                        _priorities[i].Enqueue(user);
+                        _priorities[i].Dequeue();
+                        user = _priorities[i].Peek();
+                    }
+                    if (user.requests.Count == 0)
+                    {
+                        _priorities[i].Dequeue();
+                    }
+                    else
+                    {
+                        if (user.isIO == 0 )//5|| (user.isIO < 0 && _priorities[i].Count > 1))
+                        {
+                            if (user.requests[0] - this.Tact > 0)
+                            {
+                                user.requests[0] -= this.Tact;
+
+                            }
+                            else
+                            {
+                              
+                                gaps = user.requests[0] - this.Tact;
+                                user.requests.RemoveAt(0);
+                                if (gaps <= 0)
+                                {
+                                    user.isIO = this.Io + gaps;//можно убрать gaps - не нужно
+                                   // Console.WriteLine($"Gap:{gaps}");
+                                    totalGaps -= gaps;
+                                }
+                                
+                                
+                            }
+                        }
+                        else if (user.isIO < 0)
+                        {
+                            user.isIO = 0;
+                        }
+
+                        ticks++;
+                        if (user.requests.Count == 0)
+                        {
+                            _priorities[i].Dequeue();
+                        }
+                        else
+                        {
+                            _priorities[i].Enqueue(user);
+                            _priorities[i].Dequeue();
+                        }
+
+                        
+
+
+                        for (i = i + 1; i < _priorities.Count; ++i)
+                        {
+                            this.decIo(i);
+                        }
+                        i = 0;
+                        //Console.WriteLine($"___________________________{ticks}");
+                        
+
+                        
+                    }
+
+                }
+
+                if (_priorities[i].Count == 0)
+                {
+                    _priorities.RemoveAt(i);
+                }
+            }
+
+            return (ticks, totalGaps);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 1; i <= 10; ++i)
+            {
+                for (int j = 0; j <= 10; ++j)
+                {
+                    //int len = 3;
+                    //Proccesor proc = new Proccesor(3, i, j);
+                    //proc.AddUser(0, 6, 8, 6, 3, 2, 4, 5, 3, 4, 5);
+                    //proc.AddUser(0, 5, 3, 2, 1, 2, 3, 4, 5, 6, 8);
+                    //proc.AddUser(0, 3, 4, 6, 8, 9, 5, 3, 2, 1, 2);
+                    //proc.AddUser(1, 2, 3, 2, 4, 2, 1, 1, 6, 8, 9);
+                    //proc.AddUser(1, 9, 3, 3, 4, 2, 1, 6, 7, 5, 4, 3);
+                    //proc.AddUser(1, 3, 2, 3, 1, 4, 6, 8, 5, 3, 3, 3);
+                    //proc.AddUser(2, 3, 4, 5, 2, 6, 2, 7, 8, 9, 3, 4);
+                    //proc.AddUser(2, 2, 1, 2, 3, 3, 4, 5, 6, 3, 5, 4);
+
+                    //int len = 3;
+                    //Proccesor proc = new Proccesor(3, i, j);
+                    //proc.AddUser(0, 3, 3, 4, 5, 6, 4, 2, 1, 5, 3, 4);
+                    //proc.AddUser(1, 4, 2, 5, 6, 2, 1, 1, 3, 3, 4);
+                    //proc.AddUser(1, 4, 4, 5, 6, 6, 2, 1, 1, 3, 3, 4, 4);
+                    //proc.AddUser(2, 1, 1, 4, 4, 2, 2, 4, 3, 3, 3, 3, 5, 8);
+                    //proc.AddUser(2, 4, 5, 1, 3, 3, 3, 2, 2, 4, 1, 4, 6);
+                    //proc.AddUser(2, 4, 5, 1, 1, 1, 3, 3, 3, 2, 2, 4, 1, 1);
+
+
+
+                    int len = 2;
+                    Proccesor proc = new Proccesor(2, i, j);
+                    proc.AddUser(0, 5, 3, 2, 1, 5, 2, 3, 2, 1, 6);
+                    proc.AddUser(0, 6, 8, 3, 7, 9, 3, 3, 9, 8, 7);
+                    proc.AddUser(0, 7, 6, 3, 2, 5, 3, 6, 8, 1, 3, 4);
+                    proc.AddUser(1, 4, 3, 4, 2, 1, 1, 2, 2, 3, 4, 6);
+                    proc.AddUser(1, 6, 8, 7, 3, 2, 1, 6, 3, 2, 1, 6);
+
+
+
+                    //Proccesor proc = new Proccesor(len, i, j);
+                    //proc.AddUser(0, 5, 3, 4);
+                    //proc.AddUser(1, 2, 3, 3);
+                    //proc.AddUser(1, 4, 2, 3);
+                    int sum = 0;
+                    for (int ii = 0; ii < len; ++ii)
+                    {
+                        for (int jj = 0; jj < proc[ii].Count; ++jj)
+                        {
+                            UserTimes user = proc[ii].Peek();
+                            for (int k = 0; k < user.requests.Count; ++k) //доделать
+                            {
+                                sum += user.requests[k];
+                            }
+                            proc[ii].Enqueue(user);
+                            proc[ii].Dequeue();
+                        }
+
+                    }
+
+                    Console.WriteLine($"Tatc: {i}, IO: {j}");
+                    (int ticks, int totalGaps) = proc.getConsumingTime();//посчиатть эффективное врем
+                                                                         //посчитать gaps общее - эффективное
+                    
+
+                    Console.WriteLine("Efficience: {0:F2}, Gaps: {1:D}, All: {2:D}", (double)sum / (ticks * proc.Tact), ticks * proc.Tact - sum, (ticks * proc.Tact));
+                    Console.WriteLine("-------------------------------");
+                }
+            }
+
+            
+
+
+            //Console.WriteLine(proc.getConsumingTime());
+        }
+    }
+}
